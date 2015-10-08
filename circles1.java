a451b9432ae4cddd09e9ee059c30341d9c24438c
Cusut56 <tarashere2005@yahoo.com>
2015-10-07 20:43:47 -0400

    Update circles1.java

--- circles1.java
+++ circles1.java
@@ -1,13 +1,8 @@
-//// Wild circles #1.
-
-// GLOBAL VARIABLES //
-float x,y;
-
-// Setup:  screen size, initialization. //
+/ Setup:  screen size, initialization. //
 void setup() {
   size(500, 500);        // Try changing the width & height.
-  x=  width/2;
-  y=  height/2;
+  x=  width/4;
+  y=  height/6;
 }
 
 // Next frame:  draw three circles. //
@@ -16,9 +11,9 @@ void draw() {
   ellipse(x+60, y+60, 40, 40);
   ellipse(x+90, y+90, 20, 20);
  
-  // Move them???? //--              
+            
   x=  x + 2;  y=  y + 1;
-  // Insert BREAK here ^
-  // to uncomment the above code,
+ 
+  
 }
 
